30454bc91b5cc64eb1cc97429294b339a8475245
Near Koike <nearkoike@gmail.com>
2024-03-17 19:51:07 +0800

    Gitna na

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Google from './Google'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+        <Google></Google>
+      
+    </>
+  )
+}
+
+export default App
